6dc6201021110beac46fafaa2bac742bb42d7017
Ritika0991 <ritikagoyal0991@gmail.com>
2021-04-23 00:26:39 +0530

    dd

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,25 +4,26 @@ exports.get_test = (req,res,next) => {
     var session = neo4j.session;
     var it = [];
     session
-    .run('MATCH (m:movie)-[:OF_GENRE]->(g:genre) MATCH (u:User {username:$username})-[:WATCHED]->(m:Movie) with m, count(*) as watchedOrNot return m.title as title, m.poster as poster, m.released as drelease, m.imdbRating as rating, m.duration as dur, apoc.coll.toSet(COLLECT(g.name)) as gen, watchedOrNot as wn LIMIT 25;',{
+    .run('MATCH (m:movie)-[:OF_GENRE]->(g:genre) return m.title as title, "Ritika" as poster, m.released as drelease, m.imdbRating as rating, m.duration as dur, COLLECT(g.name) as gen , 0 as wn LIMIT 25;',{
         username: user
     })
     .then(result => {
         result.records.forEach(record => {
-            it.append([{
+            it.push({
                 title: record.get('title'),
                 image: record.get('poster'),
                 date_of_release: record.get('drelease'),
                 rating: record.get('rating'),
                 watched: record.get('wn'),
                 duration: record.get('dur'),
-                genre: record.get('gen')}])
-        })
+                genre: record.get('gen')})
+        });
+        console.log(it);
         res.render('movies', {
             pageTitle: 'All Movies',
             path: '/movies',
             itlist: it
-        })
+        });
     })
     .catch(error => {
         console.log(error)
