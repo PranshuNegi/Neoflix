387dab9b90782ea905bb7d18818dbb1ba3f0a7bb
meghana <meghanab302@gmail.com>
2021-04-28 23:55:33 +0530

    rating

--- controllers/watched.js
+++ controllers/watched.js
@@ -19,7 +19,6 @@ exports.get_test = (req,res,next) => {
                 image: record.get('poster'),
                 date_of_release: record.get('drelease'),
                 rating: record.get('rating'),
-                rated: true,
                 user_rating: record.get('user_rating'),
                 duration: record.get('dur'),
                 genre: record.get('gen')})
@@ -40,5 +39,83 @@ exports.post_test = (req,res,next)=>{
     if( btype == "md"){
         umovie = mid;
         res.redirect('/details');
+    }
+    else if(btype == "unstar"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="-1";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else if(btype == "star5"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="5";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else if(btype == "star4"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="4";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else if(btype == "star3"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="3";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else if(btype == "star2"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="2";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else if(btype == "star1"){
+        var session = neo4j.session;
+        session
+        .run('MATCH (a:user {username: $username})-[r:WATCHED]->(b:movie {movieId: $movid}) SET r.rating="1";',{
+            username: user, movid: mid
+        })
+        .then(result => {
+            res.redirect('/watched');
+        })
+        .catch(error => {
+            console.log(error)
+        })
     }
 };
