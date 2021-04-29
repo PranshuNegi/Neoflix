9f905d974277c75299ebd5539eaba6f85f6630c6
Ritika0991 <ritikagoyal0991@gmail.com>
2021-04-29 19:55:56 +0530

    ss

--- controllers/recmovies.js
+++ controllers/recmovies.js
@@ -1,33 +1,6 @@
 const Movie = require('../models/movie');
 var neo4j = require('../models/neo4j');
 exports.get_test = (req,res,next) => {
-	// res.render('recmovies', {
- //        pageTitle: 'Recommended Movies',
- //        path: '/recmovies',
- //        itlist: [{
- //            title: "High School Musical 2",
- //            image: "https://m.media-amazon.com/images/M/MV5BNjI5ODI4NTc1MF5BMl5BanBnXkFtZTgwMDg3ODYzMjE@._V1_.jpg",
- //            date_of_release: "15-08-2007",
- //            rating: 4,
- //            duration: "100 mins",
- //            genre: "Musical Drama"}],
- //        itlist0:  [{
- //            title: "Zero",
- //            image: "https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2018/12/22/Photos/Processed/zero_film_review-k8AF--621x414@LiveMint.jpg",
- //            date_of_release: "21-12-2018",
- //            rating: 5.4,
- //            watched: false,
- //            duration: "164 mins",
- //            genre: "Romance Comedy"}],
- //        itlist2: [{
- //            title: "Roohi",
- //            image: "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2021/02/16/958005-rajkummarrao-varunsharma-janhvikapoor-roohi-newposter.jpg",
- //            date_of_release: "28-02-2021",
- //            rating: 7,
- //            watched: false,
- //            duration: "120 mins",
- //            genre: "Horror Comedy"}]
- //    });
  if(user == null){
         res.redirect('/login');
         return
@@ -87,3 +60,6 @@ exports.get_test = (req,res,next) => {
         console.log(error)
     })
 };
+// MATCH ( u : user { username : $username })-[ r : WATCHED ]->( m : movie ) WITH u , avg ( r . rating ) AS mean MATCH ( u )-[ r : WATCHED ]->( m : movie )-[: OF_GENRE ]->( g : genre ) WHERE r . rating >= mean WITH u , g , COUNT (*) AS score MATCH ( g )<-[: OF_GENRE ]-( m1 : movie ) WHERE NOT EXISTS (( u )-[: WATCHED ]->( m1 )) and exists(m1.imdbRating) RETURN m1.movieId as id ,m1.title as title, m1.poster as poster, m1.released as drelease, m1.imdbRating as rating, m1.duration as dur, COLLECT ( DISTINCT g.name ) as gen , SUM ( score ) AS score1, 0 as wn ORDER BY score1 DESC, m1.imdbRating DESC LIMIT 10
+
+//
