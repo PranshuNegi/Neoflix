fa122a9b2ebff337461060bfa32949336a3e6b19
meghana <meghanab302@gmail.com>
2021-05-04 22:25:05 +0530

    dataset folder deleted

--- models/neo4j.js
+++ models/neo4j.js
@@ -1,7 +1,7 @@
 var neo4j = require('neo4j-driver')
 var driver = neo4j.driver(
     'bolt://localhost',
-    neo4j.auth.basic('neo4j', 'neoflix')
+    neo4j.auth.basic('neo4j', 'megh@neo4j')
   )
   var session = driver.session({
     database: 'neoflix',
