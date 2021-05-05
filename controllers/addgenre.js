c4c4f355dfabbaa5bd5ccba91df7f6886d6177dd
PranshuNegi <pranshu.negi456@gmail.com>
2021-05-05 15:08:52 +0530

    Add and delete person and genres

--- controllers/addgenre.js
+++ controllers/addgenre.js
@@ -8,12 +8,15 @@ exports.get_test = (req,res,next) => {
     res.render('addgenre', {
         pageTitle: 'Add Genre',
         path: '/addgenre',
+        bt: null,
         status: 0
     });
 };
 
 exports.post_test = (req,res,next) => {
     const genre = req.body.genre;
+    const btype = req.body.b_type;
+    if(btype == "add"){
     var session = neo4j.session;
     session
         .run('MATCH (g:genre {name: $gname}) RETURN g.name as gn;',{
@@ -35,6 +38,7 @@ exports.post_test = (req,res,next) => {
                         pageTitle: 'Add Genre',
                         path: '/addgenre',
                         gn: genre,
+                        bt: "add",
                         status: 1
                     });
                 }
@@ -42,8 +46,53 @@ exports.post_test = (req,res,next) => {
                 res.render('addgenre', {
                     pageTitle: 'Add Genre',
                     path: '/addgenre',
+                    bt: "add",
                     status: 404
                 });
             }
         })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+    else{
+        var session = neo4j.session;
+        session
+            .run('MATCH (g:genre {name: $gname}) RETURN g.name as gn;',{
+            gname: genre
+            })
+            .then(result => {
+                if(result.records.length == 0){
+                    res.render('addgenre', {
+                        pageTitle: 'Add Genre',
+                        path: '/addgenre',
+                        bt: "del",
+                        gn: genre,
+                        status: 404
+                    });
+                }
+                else{
+                    var session2 = neo4j.session;
+                    session2
+                    .run('MATCH (n:genre {name :$ne}) DETACH DELETE n', {
+                        ne: genre
+                        })
+                        .then(result => {
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                        res.render('addgenre', {
+                            pageTitle: 'Add Genre',
+                            path: '/addgenre',
+                            gn: genre,
+                            bt: "del",
+                            status: 1
+                        });
+                }
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
 };
--- controllers/addperson.js
+++ controllers/addperson.js
@@ -9,15 +9,17 @@ exports.get_test = (req,res,next) => {
         pageTitle: 'Add Person',
         path: '/addperson',
         editing: false,
+        bt: null,
         status: 0
     });
 };
 
 exports.post_test = (req,res,next) => {
     const pid = req.body.id;
+    const btype = req.body.b_type;
+    if(btype == "add"){
     const name = req.body.name;
     const role = req.body.role;
-    console.log(role);
     var session = neo4j.session;
     session
         .run('MATCH (p:person {personId: $peid}) RETURN p.personId as pd;',{
@@ -32,6 +34,7 @@ exports.post_test = (req,res,next) => {
                         editing: true,
                         pn: name,
                         pid: pid,
+                        bt: "add",
                         status: 404
                     });
                     return;
@@ -52,6 +55,7 @@ exports.post_test = (req,res,next) => {
                                 path: '/addperson',
                                 pn: name,
                                 editing: false,
+                                bt: "add",
                                 status: 1
                      });
                      return;
@@ -72,6 +76,7 @@ exports.post_test = (req,res,next) => {
                                 path: '/addperson',
                                 pn: name,
                                 editing: false,
+                                bt: "add",
                                 status: 1
                      });
                      return;
@@ -92,6 +97,7 @@ exports.post_test = (req,res,next) => {
                                 path: '/addperson',
                                 pn: name,
                                 editing: false,
+                                bt: "add",
                                 status: 1
                      });
                      return
@@ -103,6 +109,7 @@ exports.post_test = (req,res,next) => {
                     path: '/addperson',
                     status: 403,
                     editing: true,
+                    bt: "add",
                     pn: name
                 });
             }
@@ -110,4 +117,48 @@ exports.post_test = (req,res,next) => {
         .catch(error => {
             console.log(error)
         })
+    }
+    else{
+        var session = neo4j.session;
+        session
+        .run('MATCH (p:person {personId: $peid}) RETURN p.personId as pd;',{
+            peid: pid
+        })
+        .then(result => {
+            if(result.records.length == 0){
+                    res.render('addperson', {
+                        pageTitle: 'Add Person',
+                        path: '/addperson',
+                        editing: true,
+                        pid: pid,
+                        bt: "del",
+                        status: 404
+                    });
+                    return;
+                }
+                else{
+                    var session = neo4j.session;
+                    session
+                    .run('MATCH (p:person {personId: $peid}) DETACH DELETE p',{
+                        peid: pid
+                    })
+                    .then(result => {
+                        res.render('addperson', {
+                            pageTitle: 'Add Person',
+                            path: '/addperson',
+                            editing: false,
+                            pid: pid,
+                            bt: "del",
+                            status: 1
+                        });
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+                }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
 };
